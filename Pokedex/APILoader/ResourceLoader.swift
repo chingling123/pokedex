f0f3257313dea1c8665775c3677308618d676145
eeduardo <erik.nascimento@digio.com.br>
2020-12-29 13:26:23 -0300

    move test code to production code

--- Pokedex/APILoader/ResourceLoader.swift
+++ Pokedex/APILoader/ResourceLoader.swift
@@ -15,3 +15,21 @@ enum ResourceLoaderResult {
 protocol ResourceLoader {
     func load(completion: @escaping (ResourceLoaderResult) -> Void)
 }
+
+public protocol HTTPClient {
+    func get(from url: URL)
+}
+
+public final class RemoteResourceLoader {
+    private let url: URL
+    private let client: HTTPClient
+    
+    public init(url: URL, client: HTTPClient) {
+        self.client = client
+        self.url = url
+    }
+    
+    public func load() {
+        client.get(from: url)
+    }
+}
--- PokedexTests/RemoteResourceLoader.swift
+++ PokedexTests/RemoteResourceLoader.swift
@@ -6,24 +6,7 @@
 //
 
 import XCTest
-
-class RemoteResourceLoader {
-    let client: HTTPClient
-    let url: URL
-    
-    init(url: URL, client: HTTPClient) {
-        self.client = client
-        self.url = url
-    }
-    
-    func load() {
-        client.get(from: url)
-    }
-}
-
-protocol HTTPClient {
-    func get(from url: URL)
-}
+import Pokedex
 
 class RemoteResourceLoaderTests: XCTestCase {
     
@@ -33,7 +16,7 @@ class RemoteResourceLoaderTests: XCTestCase {
         XCTAssertNil(client.requestedURL)
     }
     
-    func test_load_requestDataFromURL() {
+    func test_load_requestsDataFromURL() {
         let url = URL(string: "https://pokeapi.co/api/v2/pokemon")!
         
         let (sut, client) = makeSUT(url: url)
