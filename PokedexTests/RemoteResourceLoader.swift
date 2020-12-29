d5741516c6ad309c7649c556393d22395610b7a4
eeduardo <erik.nascimento@digio.com.br>
2020-12-29 13:50:01 -0300

    test request error

--- PokedexTests/RemoteResourceLoader.swift
+++ PokedexTests/RemoteResourceLoader.swift
@@ -37,6 +37,21 @@ class RemoteResourceLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        let url = URL(string: "https://pokeapi.co/api/v2/pokemon")!
+        
+        let (sut, client) = makeSUT(url: url)
+        client.error = NSError(domain: "Test", code: 0)
+        
+        var capturedError: RemoteResourceLoader.Error?
+        
+        sut.load { error in capturedError = error }
+        
+        XCTAssertEqual(capturedError, .connectivity)
+        
+        
+    }
+    
     // MARK: Helpers
     
     private func makeSUT(url: URL = URL(string: "https://test.com")!) -> (sut: RemoteResourceLoader, client: HTTPClientSpy) {
@@ -48,8 +63,12 @@ class RemoteResourceLoaderTests: XCTestCase {
     
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
+        var error: Error?
         
-        func get(from url: URL) {
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
     }
