1c56a24a531b0bf4ca201ab4a02917c9dc78411e
eeduardo <erik.nascimento@digio.com.br>
2020-12-29 22:51:32 -0300

    end to ent api test helpers

--- PokedexTests/ResourceAPI/APIEndToEndTests.swift
+++ PokedexTests/ResourceAPI/APIEndToEndTests.swift
@@ -11,9 +11,25 @@ import Pokedex
 class APIEndToEndTests: XCTestCase {
     
     func test_EndToEndGet() {
+        switch getFeedResult() {
+        case let .success(items)?:
+            XCTAssertEqual(items.results?.count, 20)
+        case let .failure(error)?:
+            XCTFail("Expected success, got \(error)")
+        default:
+            XCTFail("Expected success, got no irems")
+        }
+    }
+    
+    //MARK: Helpers
+    
+    private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> ResourceItemResult? {
         let url = URL(string: "https://pokeapi.co/api/v2/pokemon")!
         let client = URLSessionHTTPClient()
         let loader = RemoteResourceLoader(url: url, client: client)
+        
+        trackForMemoryLeaks(client, file: file, line: line)
+        trackForMemoryLeaks(loader, file: file, line: line)
         
         let exp = expectation(description: "Wait for load completion")
         var receivedResult: ResourceItemResult?
@@ -25,14 +41,12 @@ class APIEndToEndTests: XCTestCase {
         }
         
         wait(for: [exp], timeout: 5.0)
-        
-        switch receivedResult {
-        case let .success(items)?:
-            XCTAssertEqual(items.results?.count, 20)
-        case let .failure(error)?:
-            XCTFail("Expected success, got \(error)")
-        default:
-            XCTFail("Expected success, got no irems")
+        return receivedResult
+    }
+    
+    private func trackForMemoryLeaks(_ instance: AnyObject, file: StaticString = #file, line: UInt = #line) {
+        addTeardownBlock { [weak instance] in
+            XCTAssertNil(instance, "Instance not deallocated. Potential memory leak", file: file, line: line)
         }
     }
 }
